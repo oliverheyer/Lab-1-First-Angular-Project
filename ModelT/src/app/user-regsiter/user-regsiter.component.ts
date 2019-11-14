4ce3e03c591b71c708f6c11742ca2e842dc2b357
Oliver Heyer <54333939+oliverheyer@users.noreply.github.com>
2019-11-13 16:58:43 -0800

    Add files via upload

---
+++ ModelT/src/app/user-regsiter/user-regsiter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-regsiter',
+  templateUrl: './user-regsiter.component.html',
+  styleUrls: ['./user-regsiter.component.css']
+})
+export class UserRegsiterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
